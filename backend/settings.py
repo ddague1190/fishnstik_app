f091197f926ec2a4ca07e281b70e08b521d1d000
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2022-01-02 15:22:30 -0500

    restrict allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -212,12 +212,14 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     'https://fishnstik.herokuapp.com',
-#     'http://127.0.0.1:8000'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://fishnstik.herokuapp.com',
+    'fishnstik.herokuapp.com',
+    'http://127.0.0.1:8000',
+
+]
 
 CACHES = {
     'default': {
