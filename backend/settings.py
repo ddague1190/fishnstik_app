eecdb65521758ecf9dc2aa338168f1e92bfc485b
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2022-01-02 13:53:01 -0500

    re-run npm run build

--- backend/settings.py
+++ backend/settings.py
@@ -210,13 +210,12 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'fishnstik.herokuapp.com',
-    '127.0.0.1:8000',
-    'localhost'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'https://fishnstik.herokuapp.com',
+#     'http://127.0.0.1:8000'
+# ]
 
 CACHES = {
     'default': {
