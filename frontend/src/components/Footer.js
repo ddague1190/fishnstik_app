f6e8ed47b280e4d521544e9c99ff5b7596022732
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2022-01-02 19:48:10 -0500

    aboutus kluge

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'      
 
-function Footer() {
+function Footer() { 
     return (
         <footer>
             <Container>
                 <Row className='flex align light'>
                     <Col className="text-center py-3">Copyright &copy; Fish N Stik</Col>
                     <Col>
-                        <a href='/aboutus'>About us</a>
+                        <a href='/#/aboutus'>About us</a>
                     </Col>
                 </Row>
             </Container>
