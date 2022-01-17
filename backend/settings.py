250808c4c2bdcac5f00572d9fdf9c603ae6e3e8a
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2022-01-17 08:20:51 -0500

    staticfilesdirectory update

--- backend/settings.py
+++ backend/settings.py
@@ -205,7 +205,8 @@ MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'frontend/build/static'
+    BASE_DIR / 'frontend/build/static',
+    BASE_DIR / 'frontend/build/static/media'
 ]
 
 # Upload images here
