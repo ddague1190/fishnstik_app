1b5561534b3c924338c2e17ed7268bbe6a3e691c
Darryl Dague <darryldague@Darryls-MacBook-Air.local>
2022-01-27 05:47:15 -0500

    configure path to base template

--- backend/urls.py
+++ backend/urls.py
@@ -5,15 +5,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from payments.views import ProcessWebhookView
 from django.views.generic import TemplateView
+from django.urls import re_path
+
 
 urlpatterns = [
 
     path('fns192837465/', admin.site.urls),
-    path('api/products', include('base.urls.product_urls')),
+    path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
     path('webhooks/paypal/', ProcessWebhookView.as_view()),
-    path('', include('base.urls.frontend_urls')),
+    re_path('', TemplateView.as_view(template_name='index.html')),
 ] 
 
 
