3fddacdd210bc4586c11a263d76284db197fa503
Darryl Dague <darryldague@Darryls-MacBook-Air.local>
2022-01-27 05:39:02 -0500

    re-configure base template renderer

--- backend/urls.py
+++ backend/urls.py
@@ -9,20 +9,14 @@ from django.views.generic import TemplateView
 urlpatterns = [
 
     path('fns192837465/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
     path('api/products', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
-    path('webhooks/paypal/', ProcessWebhookView.as_view())
-
+    path('webhooks/paypal/', ProcessWebhookView.as_view()),
+    path('', include('base.urls.frontend_urls')),
 ] 
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += [
-    # match the root
-    path(r'^$', TemplateView.as_view(template_name='index.html')),
-    # match all other pages
-    path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
-]
+
